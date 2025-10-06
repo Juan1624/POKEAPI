c5b699e50c1171b0a9aa7cb0cb3db01bef9461fc
Juan1624 <juanestebanarias164@gmail.com>
2025-10-06 18:34:24 +0000

    clase 29/09

--- js/conexion.js
+++ js/conexion.js
@@ -1,20 +1,38 @@
 let pokemones = [];
-let totalPokes = 10;
+let totalPokes = 1025;
 
-// Conexión para obtener la lista de Pokémon
-async function conexionLista() {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${totalPokes}`);
-  const data = await res.json();
-  return data.results;
+//conexion para obtener la lista de pokemon
+
+async function Conexion(UnFiltro) {
+    if(UnFiltro == "All"){
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${totalPokes}`);
+    const data = await res.json();
+    return data.results;
+  }else{
+    const res = await fetch(`https://pokeapi.co/api/v2/type/${UnFiltro}`);
+    const data = await res.json();
+    const pokemonesTipo = [];
+    for (let i = 0; i < data.pokemon.length; i++) {
+      pokemonesTipo.push(data.pokemon[i].pokemon);
+    }
+    return pokemonesTipo;
+    }
 }
 
-// Cargar todos los Pokémon al iniciar
+//cargar todos los pokemon al iniciar
+
 async function General() {
   if (pokemones.length === 0) {
-    pokemones = await conexionLista();
+    pokemones = await Conexion("All");
   }
-  Home(pokemones)
-  console.log (pokemones[2].name)
+  Home();
 }
 
 General()
+
+async function FiltroConexion(filtroelegido){
+    pokesFiltrados = await Conexion(filtroelegido)
+    document.getElementById("la-lista").innerHTML = "";
+    listaFiltro = GenerarLista(pokesFiltrados)
+    document.getElementById("la-lista").innerHTML = listaFiltro
+}
